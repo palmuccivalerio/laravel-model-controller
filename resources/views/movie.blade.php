7c605d48b1128f009830067d11a5ef7fee6e1961
palmuccivalerio <palmuccivalerio@gmail.com>
2024-06-10 19:38:28 +0200

    add:new

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,21 +1,9 @@
 @extends('layouts.app');
 @section('content')
 <div class="d-flex flex-wrap gap-3 justify-content-center">
-    
+
     @foreach ($movies as $movie)
-
-    
-
-
-    <div class="card col-4" style="width: 18rem;">
-        <img class="card-img-top" src="{{$movie['image']}}" alt="Card image cap">
-        <div class="card-body">
-            <p class="card-text"> <strong>Titolo</strong> : {{$movie['title']}}</p>
-            <p class="card-text">{{$movie['original_title']}}</p>
-            <p class="card-text">{{$movie['vote']}}/10</p>
-        </div>
-    </div>
-
-@endforeach
+        @include ('partials/card')
+    @endforeach
 </div>
 @endsection
---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,8 @@
+<div class="card col-4" style="width: 18rem;">
+        <img class="card-img-top" src="{{$movie['image']}}" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text"> <strong>Titolo</strong> : {{$movie['title']}}</p>
+            <p class="card-text">{{$movie['original_title']}}</p>
+            <p class="card-text">{{$movie['vote']}}/10</p>
+        </div>
+    </div>
